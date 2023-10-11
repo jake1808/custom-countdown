b961428443b6f6a6f7b186623649ef4e8f58b4f9
jake1808 <mutale1808@gmail.com>
2023-10-11 10:25:46 +0200

    gained access to form elements

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 const inputContainer = document.getElementById('input-container');
-const countdownForm = document.getElementById('countdownForm');
+const countdownForm = document.getElementById('form');
 const dateEl = document.getElementById('date-picker');
+
+let countdownTitle = '';
+let countdownDate = '';
 
 // set date input min with today's date
 let today = new Date().toISOString().split('T')[0];
 dateEl.setAttribute('min', today);
+
+
+// Take values from form input
+function updateCountdown(e) {
+    e.preventDefault();
+    countdownTitle = e.srcElement[0].value;
+    countdownDate = e.srcElement[1].value;
+
+}
+
+// Event listener
+countdownForm.addEventListener('submit', updateCountdown);
