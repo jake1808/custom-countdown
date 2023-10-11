d293d43ce163b00cd586260e7234bc94485cff4f
jake1808 <mutale1808@gmail.com>
2023-10-11 11:06:11 +0200

    Updated the DOM with the countdown times

--- script.js
+++ script.js
@@ -2,20 +2,57 @@ const inputContainer = document.getElementById('input-container');
 const countdownForm = document.getElementById('form');
 const dateEl = document.getElementById('date-picker');
 
+const countdownEl = document.getElementById('countdown');
+const countdownElTitle = document.getElementById('countdown-title');
+const countdownBtn = document.getElementById('countdown-button');
+const timeElements = document.querySelectorAll('span');
+
 let countdownTitle = '';
 let countdownDate = '';
+let countdownValue = Date;
+
+const second = 1000
+const minute = second * 60
+const hour = minute * 60
+const day = hour * 24
 
 // set date input min with today's date
 let today = new Date().toISOString().split('T')[0];
 dateEl.setAttribute('min', today);
 
 
+// Populate Countdown / Complete UI
+function updateDOM() {
+    const now = new Date().getTime();
+    const distance = countdownValue - now;
+
+    const days = Math.floor(distance / day);
+    const hours = Math.floor((distance % day) / hour)
+    const minutes = Math.floor((distance % hour) / minute)
+    const seconds = Math.floor((distance % minute) / second)
+    console.log(days, hours, minutes, seconds)
+
+    // Populate countdown
+    countdownElTitle.textContent = `${countdownTitle}`;
+    timeElements[0].textContent = `${days}`
+    timeElements[1].textContent = `${hours}`
+    timeElements[2].textContent = `${minutes}`
+    timeElements[3].textContent = `${seconds}`
+
+    // Hide Input
+    inputContainer.hidden = true
+    // show countdown
+    countdownEl.hidden = false
+}
+
 // Take values from form input
 function updateCountdown(e) {
     e.preventDefault();
     countdownTitle = e.srcElement[0].value;
     countdownDate = e.srcElement[1].value;
-
+    // get number version of current Date
+    countdownValue = new Date(countdownDate).getTime();
+    updateDOM();
 }
 
 // Event listener
